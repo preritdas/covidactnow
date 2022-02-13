7c0c8511d3463df8275889a7d81236ab0150596c
Prerit Das <preritdas@gmail.com>
2022-02-13 01:59:17 -0800

    Added docstrings to all classes and functions. In sync with version 1.1.75.

--- README.md
+++ README.md
@@ -23,5 +23,7 @@ washingtonInfectionRate = 1.16
 massachussettsVaxRate = 76.3
 ```
 
+To view the docstring (explainer) of a particular function, use `User.infRate.__doc__` where `infRate` can be repalced with any other function. For example, running `print(User.freeBedPercentage.__doc__)` will return: `Get's the percentage of free hospital beds by state.` Depending on your IDE, hovering over a function in the editor will show the function's docstring (works in VS Code).
+
 ----
 For more information on how the wrapper works, particularly how it was redesigned to be package-friendly, read the "Updated for Distribution" section of this [page](https://covidinfo.preritdas.com). Note that this version has been updated to get data from within a `User` class allowing individual API keys to be used (as opposed to defining a state as an object and defining statistics as object attributes).
--- covidactnow/covid.py
+++ covidactnow/covid.py
@@ -3,13 +3,17 @@ import requests
 # As CovidActNow's API is a REST API, queries are stored as URLs.
 
 class User:
+    """Main User class taking api_key with attributes to get COVID statistics."""
+    
     def __init__(self, api_key):
         self.api_key = api_key
 
     def queryUrl(self, state):
+        """Returns the query url for requests based on statistic."""
         return f"http://api.covidactnow.org/v2/state/{state}.json?apiKey=" + self.api_key
 
     def infRate(self, state):
+        """Gets today's COVID infection rate by state."""
         # Infection Rate
         try:
             return float(requests.get(self.queryUrl(state)).json()['metrics']['infectionRate'])
@@ -18,7 +22,8 @@ class User:
         except Exception as e:
             print(f"An error occured in data collection: {e}")
         
-    def posRate(self, state):    
+    def posRate(self, state):
+        """Gets today's COVID positive testing rate by state."""    
         # Positive Rate
         try:
             return round(100*float(requests.get(self.queryUrl(state)).json()['metrics']['testPositivityRatio']), 2)
@@ -28,6 +33,7 @@ class User:
             print(f"An error occured in data collection: {e}")
     
     def vaxRate(self, state):
+        """Get's the vaccination rate as of today, by state."""
         # Vax Rate
         try:
             return round(100*float(requests.get(self.queryUrl(state)).json()['metrics']['vaccinationsCompletedRatio']), 2)
@@ -37,6 +43,7 @@ class User:
             print(f"An error occured in data collection: {e}")
     
     def freeBedPercentage(self, state):
+        """Get's the percentage of free hospital beds by state."""
         # Free Bed Percentage
         try:
             return round(100*float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['currentUsageTotal'])/\
@@ -47,6 +54,8 @@ class User:
             print(f"An error occured in data collection: {e}")
         
     def newCases(self, state):
+        """Gets the number of new cases today by state."""
+        # New Cases
         try:
             return int(requests.get(self.queryUrl(state)).json()['actuals']['newCases'])
         except TypeError:
@@ -55,6 +64,8 @@ class User:
             print(f"An error occured in data collection: {e}")
 
     def newDeaths(self, state):
+        """Gets the number of new deaths today, by state."""
+        # New Deaths
         try:
             return int(requests.get(self.queryUrl(state)).json()['actuals']['newDeaths'])
         except TypeError:
@@ -63,6 +74,8 @@ class User:
             print(f"An error occured in data collection: {e}")
         
     def covidBedPercentage(self, state):
+        """Gets the percentage of hospital beds occupied by COVID patients."""
+        # Covid Bed Percentage
         try:
             return round(100*float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['currentUsageCovid'])/\
                 float(requests.get(self.queryUrl(state)).json()['actuals']['hospitalBeds']['capacity']), 2)
